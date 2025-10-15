720acca905161960b5841a0e8b8900548117e835
Deniz Can Özder <candenizozder08@gmail.com>
2025-10-15 16:32:27 +0300

    Cookie Set

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -13,6 +13,14 @@ export const registerUserController = async (req, res) => {
 export const loginUserController = async (req, res) => {
   const userData = req.body;
   const session = await loginUser(userData);
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
   res.status(200).send({
     mesaj: 'Access Token',
     data: session,
