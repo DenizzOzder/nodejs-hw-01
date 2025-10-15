0a5a72ea6eb340fd24dac397522a07a211e2f35d
Deniz Can Özder <candenizozder08@gmail.com>
2025-10-15 15:01:32 +0300

    Bcrpyt & Pass del

--- src/models/user.js
+++ src/models/user.js
@@ -4,14 +4,31 @@ const { Schema } = mongoose;
 const userSchema = new Schema(
   {
     name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    // select:false -> JSON/queries'de default gizli olur (login'de +password ile seçersin)
+    password: { type: String, required: true, select: false },
   },
   {
     timestamps: true,
     versionKey: false,
   },
 );
+
+// JSON'a çevrilirken password'u sil
+userSchema.set('toJSON', {
+  transform: function (doc, ret) {
+    delete ret.password;
+    return ret;
+  },
+});
+
+// (opsiyonel) toObject için de aynı kuralı istersen:
+userSchema.set('toObject', {
+  transform: function (doc, ret) {
+    delete ret.password;
+    return ret;
+  },
+});
 
 const UserCollection = mongoose.model('Users', userSchema, 'users');
 export default UserCollection;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,16 @@
+import createHttpError from 'http-errors';
 import UserCollection from '../models/user.js';
+import bcrypt from 'bcrypt';
 
 export const registerUser = async (userData) => {
+  const { name, email, password } = userData;
+  const userCheck = await UserCollection.findOne({ email });
+  /* Kontrol */
+  if (userCheck) {
+    throw createHttpError(409, 'Bu email hali hazırda mevcut');
+  }
+  const passCrypt = await bcrypt.hash(password, 10);
+  userData.password = passCrypt;
   const user = await UserCollection.create(userData);
   return user;
 };
